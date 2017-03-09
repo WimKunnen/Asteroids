a71e6f44f15654529016399f2f23155fce37ff8f
Unknown <maartendoclo@hotmail.com>
2017-03-09 16:37:35 +0100

    Changes to testShip

--- AsteroidsOGP/tests/asteroids/tests/TestShip.java
+++ AsteroidsOGP/tests/asteroids/tests/TestShip.java
@@ -29,16 +29,18 @@ public class TestShip {
 	
 	@Before
 	public void setUp() {
+		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
+		ship1.setMaximumVelocity(100000.0);
 		ship2 = new Ship(0.0,0.0,10.0,10.0,30.0,0.0);
+		ship2.setMaximumVelocity(300010);
 		ship3 = new Ship(100.0,0.0,0.0,0.0,30,0.0);
 		ship4 = new Ship(30.0,0.0,10.0,0.0,10.0,0.0);
 	}
 
 	@Test
-	public void setMaximumVelocity_Legal() {
-		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
-		ship1.setMaximumVelocity(100000.0);
+	public void testInitializeShip() {
 		assertEquals(100000.0,ship1.getMaximumVelocity(),EPSILON);
+		assertEquals(300000.0, ship2.getMaximumVelocity(),EPSILON);
 		assertEquals(1.5,ship1.getPosition().getX(),EPSILON);
 		assertEquals(15.0,ship1.getPosition().getY(),EPSILON);
 		assertEquals(10.0,ship1.getVelocity().getX(),EPSILON);
@@ -49,8 +51,6 @@ public class TestShip {
 
 	@Test
 	public void isValidAngle_LegalCase() {
-		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
-		ship1.setMaximumVelocity(100000.0);
 		assertTrue(ship1.isValidAngle(Math.PI));
 		assertTrue(ship1.isValidAngle(0));
 
@@ -58,24 +58,18 @@ public class TestShip {
 	
 	@Test
 	public void isValidAngle_IllegalCase() {
-		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
-		ship1.setMaximumVelocity(100000.0);
 		assertFalse(ship1.isValidAngle(-Math.PI));
 		assertFalse(ship1.isValidAngle(3*Math.PI));
 	}
 	
 	@Test(expected=IllegalArgumentException.class)
 	public void radius_IllegalCase() throws Exception {
-		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
-		ship1.setMaximumVelocity(100000.0);
 		ship1 = new Ship(1.5,15.0,10.0,20.0,5.0,Math.PI);
 				
 	}
 	
 	@Test
 	public void isValidRadiusTest() {
-		ship1 = new Ship(1.5,15.0,10.0,20.0,30.0, Math.PI);
-		ship1.setMaximumVelocity(100000.0);
 		assertTrue(ship1.isValidRadius(10));
 		assertTrue(ship1.isValidRadius(100));
 		assertFalse(ship1.isValidRadius(5));
@@ -83,7 +77,6 @@ public class TestShip {
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void move_IllegalCase() throws Exception {
-		ship1 = new Ship(0.0,0.0,10.0,10.0,30.0,Math.PI);
 		ship1.move(-5);
 	}
 	

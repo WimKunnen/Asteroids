0d6fd83f33410c190abf91f440c67f88338a383f
Unknown <wim.kunnen@student.kuleuven.be>
2017-04-24 14:02:33 +0200

    Fix Resources

--- AsteroidsOGP/src/asteroids/model/program/expressions/BooleanExpression.java
+++
@@ -1,8 +0,0 @@
-package asteroids.model.program.expressions;
-
-/**
- * Created by WimKunnen on 23/04/2017.
- */
-public class BooleanExpression {
-
-}
---
+++ AsteroidsOGP/src/asteroids/model/program/expressions/BooleanLiteral.java
@@ -0,0 +1,8 @@
+package asteroids.model.program.expressions;
+
+/**
+ * Created by WimKunnen on 23/04/2017.
+ */
+public class BooleanLiteral {
+
+}
